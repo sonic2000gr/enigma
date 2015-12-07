1d03edaa614067485b130884d0bdc4b5fd203121
Manolis Kiagias <sonicy@otenet.gr>
2015-12-07 11:16:36 +0200

    Alphabet example, second version

--- arduino/serial-alphabet/alphabet/alphabet.ino
+++ arduino/serial-alphabet/alphabet/alphabet.ino
@@ -1,20 +1,42 @@
+/* Mini Enigma 
+   Version 0.1
+   (C) 2015 Manolis Kiagias
+   The Enigma Project 
+   Licensed under the BSD license */
+
+
 void setup() {
-  // put your setup code here, to run once:
+ 
+ /* Initialize serial communications */
+  
  Serial.begin(9600);
  Serial.println("Enigma Ready");
+ 
+ /* Initialize outputs 8 to 12
+    Connect five LEDs to outputs D8 - D12
+    via 330 Ohm resistors to GND */
+    
  for (int i=8; i<=12; i++)
    pinMode(i,OUTPUT);
+ 
+ Serial.print("Enter letter: ");
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
   char c;
+  
+  /* Read letter from serial if available */
+  
   if (Serial.available()) {
-    Serial.println("Enter letter:");
-    c=Serial.read();
+    c = Serial.read();
     Serial.println(c); 
-    Serial.print("Result:");
+    Serial.print("Result: ");
+    
+    /* Convert to binary and display */
+    
     showInt(c);
+    Serial.print("Enter letter: ");
   }
 }
 
@@ -22,28 +44,48 @@ void showInt(int c)
 {
   char s[6];
   int i=0;
-  c = c - 64;
-  if (c>=0 && c<=26) {
-  for (i=0; i<=4; i++)
-    s[i]='0';
-  s[5]='\0';
-  i=4;
-  while (c>=1) {
-    if  (c % 2==1)
-      s[i--]='1';
-    else
-      s[i--]='0';
-    c = c / 2;
-  }
+ 
+  /* convert lower case to upper case if needed */
+ 
+  if (c >= 95 && c <= 122)
+    c = c - 96;
+  else
+    c = c - 64;
+ 
+  /* if not in letter range, do not encode */
+ 
+  if (c >= 1 && c <= 26) {
+    /* initialize array */
+ 
+    for (i = 0; i <= 4; i++)
+      s[i]='0';
+ 
+    /* Terminate char array properly to be printed
+      by Serial.println */
+      
+    s[5]='\0';
+    
+    /* Convert to binary */
+    
+    i=4;
+    while (c >= 1) {
+      if  (c % 2 == 1)
+        s[i--] = '1';
+      else
+        s[i--] = '0';
+      c = c / 2;
+    }
   
-  for (i=8; i<=12; i++)
-    if  (s[i-8]=='0')
-      digitalWrite(i, LOW);
-    else
-      digitalWrite(i, HIGH);
+    /* Display in LEDs 
+       Outputs to D8-D12 */
+       
+    for (i = 8; i <= 12; i++)
+      if  (s[i-8] == '0')
+        digitalWrite(i, LOW);
+      else
+        digitalWrite(i, HIGH);
       
-  Serial.println(s);
+    Serial.println(s);
   } else
-  Serial.println("Error!");
+    Serial.println("Error!");
 }
-      
